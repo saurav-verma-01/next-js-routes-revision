a17b8ec633dc7b69e7eacc83fb71b3c4ad05a7d9
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-11 02:32:05 +0530

    revision completed

--- app/(dashboard)/projects/list/page.tsx
+++ app/(dashboard)/projects/list/page.tsx
@@ -1,13 +1,14 @@
 // import React from 'react'
 
 import Link from "next/link";
+import styles from "./projectlist.module.css";
 
 const page = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <h1>This is My Projects List</h1>
 
-      <ul>
+      <ul className={styles.projectlist}>
         <li>
           <Link href="/projects/jobit">Jobit</Link>
         </li>
